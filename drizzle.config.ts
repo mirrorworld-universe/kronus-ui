85146d99f5a80e59ba30b345b3f3824357705e51
Data Salaryman <datasalaryman@protonmail.com>
2025-07-25 12:35:35 +0800

    build: use nuxt db url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./server/db/schema.ts",
   // driver: "pg",
   dbCredentials: {
-    url: "postgresql://postgres:postgres@127.0.0.1:54322/postgres"
+    url: process.env.NUXT_DATABASE_URL!,
   }
 });
